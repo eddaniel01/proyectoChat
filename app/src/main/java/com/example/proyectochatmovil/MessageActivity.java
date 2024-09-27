80987956dc6378fba5edd7983d7c8ba432b8e28d
eddaniel01 <edrb0001@ce.pucmm.edu.do>
2024-09-27 17:57:38 -0400

    Enviando mensajes
    Signed-off-by: eddaniel01 <edrb0001@ce.pucmm.edu.do>

--- app/src/main/java/com/example/proyectochatmovil/MessageActivity.java
+++ app/src/main/java/com/example/proyectochatmovil/MessageActivity.java
@@ -3,7 +3,10 @@ package com.example.proyectochatmovil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
@@ -23,6 +26,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -33,7 +39,11 @@ public class MessageActivity extends AppCompatActivity {
     FirebaseUser fuser;
     DatabaseReference reference;
 
+    ImageButton btn_enviar;
+    EditText texto_enviado;
+
     Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +64,28 @@ public class MessageActivity extends AppCompatActivity {
 
         imagen_deperfil = findViewById(R.id.imagen_deperfil);
         username = findViewById(R.id.username);
+        btn_enviar = findViewById(R.id.btn_enviar);
+        texto_enviado = findViewById(R.id.texto_enviado);
 
         intent = getIntent();
-        String userid = intent.getStringExtra("userid");
-
+        final String userid = intent.getStringExtra("userid");
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String msj = texto_enviado.getText().toString();
+                if (!msj.equals("")){
+                    enviarMensaje(fuser.getUid(),userid, msj);
+                }else {
+                    Toast.makeText(MessageActivity.this, "No puedes enviar mensajes sin contenido.", Toast.LENGTH_SHORT).show();
+                }
+                texto_enviado.setText("");
+            }
+        });
+
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -88,5 +115,17 @@ public class MessageActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void enviarMensaje (String emisor, String receptor, String mensaje){
+
+        DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference();
+        HashMap <String, Object> hashMap = new HashMap<>();
+
+        hashMap.put("emisor", emisor);
+        hashMap.put("receptor", receptor);
+        hashMap.put("mensaje", mensaje);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
